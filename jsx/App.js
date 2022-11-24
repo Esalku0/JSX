608fbbb7549457f46f663d05c4ef1db2d5b839aa
ALUMNADO\esalku <esalku@floridauniversitaria.es>
2022-11-24 18:24:02 +0100

    commit inicial

--- jsx/App.js
+++ jsx/App.js
@@ -5,16 +5,9 @@
  * @format
  * @flow strict-local
  */
-import { Surface } from 'react-native-paper';
-import { TextInput } from 'react-native-paper';
-import { Button } from 'react-native-paper';
-import { Switch } from 'react-native-paper';
-import { Chip } from 'react-native-paper';
-import { useState, useEffect } from 'react';
-import { DateTimePickerModal } from 'react-native-paper-datetimepicker';
-import { Provider as PaperProvider } from 'react-native-paper';
 
 import React from 'react';
+import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -24,7 +17,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-//manel?
+
 import {
   Colors,
   DebugInstructions,
@@ -32,10 +25,10 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-//commit
+
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
-const Section = ({ children, title }) => {
+const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -61,34 +54,44 @@ const Section = ({ children, title }) => {
   );
 };
 
-const App = (props) => {
-  const isDarkMode = useColorScheme() === 'dark';;
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-
-  };
-  const saluda = nom => {
-    return (
-      <Text style={{ color: 'blue', fontSize: 25 }}> hola {nom}</Text>
-    );
-  };
-  const saluda2 = () => {
-    return (
-      <View>
-        {saluda('tani')}
-        {saluda('rafeta')}
-      </View>
-    );
   };
 
-
-  const esPersona = true;
   return (
-
-    <PaperProvider>
-      {(esPersona) ? saluda('santi programaor') : saluda('piuet grosset')}
-      {saluda2()}
-    </PaperProvider>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -96,10 +99,6 @@ const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
-  },
-  width: {
-    width: 340,
-    alignSelf: 'center',
   },
   sectionTitle: {
     fontSize: 24,
@@ -113,22 +112,6 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
-  chip1: {
-    width: 95,
-    textAlign: 'center',
-  },
-  chip2: {
-    width: 70,
-    textAlign: 'center',
-  },
-  surface: {
-    padding: 8,
-    height: 80,
-    width: 80,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
 
 export default App;
-
